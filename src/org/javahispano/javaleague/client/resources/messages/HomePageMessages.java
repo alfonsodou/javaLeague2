9f8184fc8554287eba51dcaf3fda4c7c34bb2339
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2014-06-19 15:27:44 +0200

    Creación ficheros mensajes de la página principal

---
+++ src/org/javahispano/javaleague/client/resources/messages/HomePageMessages.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.javahispano.javaleague.client.resources.messages;
+
+import com.google.gwt.i18n.client.LocalizableResource.DefaultLocale;
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * @author adou
+ *
+ */
+@DefaultLocale("es")
+public interface HomePageMessages extends Messages {
+	@DefaultMessage("2014 :: framework javaCup es un proyecto opensource con licencia GPL v3")
+	String gpl();
+
+}

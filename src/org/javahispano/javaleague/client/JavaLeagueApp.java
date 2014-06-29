92c0e1e48de57c6315bbfc7f8b1ed7e97e5fd58d
adou <alfonsodou@gmail.com>
2014-06-29 23:04:45 +0200

    Modificando entrypoint

--- src/org/javahispano/javaleague/client/JavaLeagueApp.java
+++ src/org/javahispano/javaleague/client/JavaLeagueApp.java
@@ -3,50 +3,45 @@
  */
 package org.javahispano.javaleague.client;
 
+import org.javahispano.javaleague.client.mvp.AppPlaceHistoryMapper;
+import org.javahispano.javaleague.client.mvp.ListsPlace;
+
 import com.google.common.eventbus.EventBus;
+import com.google.gwt.activity.shared.ActivityManager;
+import com.google.gwt.activity.shared.ActivityMapper;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.place.shared.PlaceHistoryHandler;
-import com.google.gwt.place.shared.PlaceHistoryMapper;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author adou
  * 
  */
 public class JavaLeagueApp implements EntryPoint {
-    private ClientFactory clientFactory = GWT.create(ClientFactory.class);
-    private Place defaultPlace = new ListsPlace();
 
     @Override
     public void onModuleLoad()
     {
-            /**
-             * This is the entry point method.
-             */
-            // Create ClientFactory using deferred binding so we can replace with
-            // different
-            // impls in gwt.xml
-            EventBus eventBus = clientFactory.getEventBus();
-            PlaceController placeController = clientFactory.getPlaceController();
-            ListwidgetApp app = clientFactory.getApp();
-            Widget appWidget = app.getAppWidget();
+        ClientFactory clientFactory = GWT.create(ClientFactory.class);
+        EventBus eventBus = clientFactory.getEventBus();
+        PlaceController placeController = clientFactory.getPlaceController();
 
-            // Start PlaceHistoryHandler with our PlaceHistoryMapper
-            PlaceHistoryMapper historyMapper = clientFactory.getHistoryMapper();
-            PlaceHistoryHandler historyHandler = new PlaceHistoryHandler(historyMapper);
-            historyHandler.register(placeController, eventBus, defaultPlace);
+        // Start ActivityManager for the main widget with our ActivityMapper
+        ActivityMapper activityMapper = new AppActivityMapper(clientFactory);
+        ActivityManager activityManager = new ActivityManager(activityMapper, eventBus);
+        activityManager.setDisplay(appWidget);
 
-            RootPanel.get("gwt").add(appWidget);
-            // Goes to place represented on URL or default place
-            historyHandler.handleCurrentHistory();
-            
-            // Hide wait GIF
-            DOM.removeChild(RootPanel.getBodyElement(), DOM.getElementById("loading"));
+        // Start PlaceHistoryHandler with our PlaceHistoryMapper
+        AppPlaceHistoryMapper historyMapper= GWT.create(AppPlaceHistoryMapper.class);
+        PlaceHistoryHandler historyHandler = new PlaceHistoryHandler(historyMapper);
+        historyHandler.register(placeController, eventBus, defaultPlace);
+
+        RootPanel.get().add(appWidget);
+        // Goes to the place represented on URL else default place
+        historyHandler.handleCurrentHistory();
     }
 
 	private String buildStackTrace(Throwable t, String log) {
